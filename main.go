048b33ee08990cbc6de5d6d6473b785e970b77be
OdinsPlasmaRifle <generalaetius.jbvb@gmail.com>
2017-06-10 23:34:00 +0200

    Added non functionalit boltdb tx

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"github.com/boltdb/bolt"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
@@ -18,6 +19,9 @@ var interval = 60
 // CLI Flags.
 var confFile = flag.String("config", "", "Config file")
 
+// Database
+var Db *bolt.DB
+
 func main() {
 	log.Println("Loading...")
 
@@ -25,17 +29,24 @@ func main() {
 	conf, err := load(*confFile)
 
 	if err != nil {
-		log.Fatalf("Unable to load services! Error: %v", err)
-	} else {
-		log.Println("Successfuly loaded, switching to monitoring mode!")
-		conf.monitor()
+		log.Fatalf("Unable to load configuration! Error: %v", err)
 	}
+
+	Db, err := conf.connect()
+	if err != nil {
+		log.Fatalf("Unable to connect to database! Error: %v", err)
+	}
+	defer Db.Close()
+
+	log.Println("Successfuly initiated, switching to monitoring mode!")
+	conf.monitor()
 }
 
 // Configuration object.
 type config struct {
-	Interval int      `yaml:"interval"`
-	Services services `yaml:"services"`
+	DatabaseFile string   `yaml:"database_file"`
+	Interval     int      `yaml:"interval"`
+	Services     services `yaml:"services"`
 }
 
 // List of multiple services.
@@ -74,17 +85,30 @@ func load(confFile string) (config, error) {
 		return conf, yamlErr
 	}
 
+	// Set database file
+	if conf.DatabaseFile == "" {
+		conf.DatabaseFile = "statter.db"
+	}
+
 	// Set default interval
 	if conf.Interval == 0 {
 		conf.Interval = interval
 	}
 
-	// Faile if no services are defined
+	// Fail if no services are defined
 	if len(conf.Services) == 0 {
 		return conf, errors.New("No services to monitor.")
 	}
 
 	return conf, nil
+}
+
+func (conf config) connect() (*bolt.DB, error) {
+	db, err := bolt.Open(conf.DatabaseFile, 0666, &bolt.Options{Timeout: 1 * time.Second})
+
+	// Initiate Buckets
+
+	return db, err
 }
 
 // Start up monitoring for sevices, instantiates a set of times to trigger
@@ -112,7 +136,7 @@ type monitorMessage struct {
 	error error
 }
 
-// Iterate through each service and trigger a test task. Creates a monotoring
+// Iterate through each service and trigger a test task. Creates a monitoring
 // channel to catch goroutine errors.
 func (ss services) iterate() {
 	monitorTask := make(chan monitorMessage)
@@ -148,8 +172,27 @@ func (s service) test(monitorTask chan<- monitorMessage) {
 	if message.error != nil {
 		monitorTask <- monitorMessage{error: message.error}
 	} else {
-		log.Println(fmt.Sprintf("Test: %v %v", s.Url, message.data.Status))
-		monitorTask <- monitorMessage{}
+		// Insert into db with time key and URL bucket
+		err := Db.Update(func(tx *bolt.Tx) error {
+			bucket, err := tx.CreateBucketIfNotExists([]byte(s.Url))
+			if err != nil {
+				return err
+			}
+
+			t := time.Now().Format(time.RFC3339)
+			err = bucket.Put([]byte(t), []byte(message.data.Status))
+			if err != nil {
+				return err
+			}
+			return nil
+		})
+
+		if err != nil {
+			monitorTask <- monitorMessage{error: err}
+		} else {
+			log.Println(fmt.Sprintf("Test: %v %v", s.Url, message.data.Status))
+			monitorTask <- monitorMessage{}
+		}
 	}
 }
 
